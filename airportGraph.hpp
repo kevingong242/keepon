165ed5ce39003f74bb1e6a36e121e02665bbc586
Kevin Gong <kevingong1995@gmail.com>
2018-03-26 00:12:18 -0700

    Included text file with airports and header file

---
+++ airportGraph.hpp
@@ -0,0 +1,16 @@
+#ifndef AIRPLANEGRAPH_HPP
+#define AIRPLANEGRAPH_HPP
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+
+struct Airport{
+
+};
+
+struct AirportEdge{
+
+};
